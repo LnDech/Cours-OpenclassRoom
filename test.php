bfa67c328a89e39d0dbdcb46b4e615576cae6690
LnDech <helene.dechelette@proton.me>
2023-04-29 14:47:39 +0200

    init

---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Page de thest PHP</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+</head>
+<body>
+<?php
+echo "<ul>
+    <li>Nom: $_POST['nom']</li>
+    <li>Prenom: $_POST['prenom']</li>
+    <li>Age: $_POST['age']</li>
+</ul>";
+?>
+</body>
+</html>
